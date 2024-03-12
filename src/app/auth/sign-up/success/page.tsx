74a784587d7377bc5b40a3aca63143efcdf3a773
spike76 <spike76@qq.com>
2024-03-12 16:39:53 +0800

    feat: auth success page

---
+++ src/app/auth/sign-up/success/page.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { Flex, Typography, Image } from 'antd';
+import { Button } from '@/components/antd';
+import colors from '@/theme/colors';
+import { useTranslation } from '@/i18n/client';
+
+const Page: React.FC = () => {
+  const { t } = useTranslation('auth-page');
+
+  return (
+    <>
+      <Flex vertical justify="center" align="center">
+        <Typography.Title
+          style={{
+            fontSize: '22px',
+            marginTop: '64px'
+          }}
+        >
+          {t('signUp.success.title')}
+        </Typography.Title>
+        <Typography.Text
+          style={{
+            whiteSpace: 'pre-wrap',
+            textAlign: 'center'
+          }}
+        >
+          {t('signUp.success.description')}
+        </Typography.Text>
+        <Image
+          src="/images/phone.png"
+          style={{
+            marginLeft: '-10%',
+            marginTop: '62px'
+          }}
+        />
+      </Flex>
+
+      <Flex
+        style={{
+          position: 'fixed',
+          bottom: 0,
+          width: '100%',
+          borderTop: '1px solid #D9D9D9',
+          padding: '16px 20px',
+          boxSizing: 'border-box',
+          backgroundColor: colors.white
+        }}
+      >
+        <Button
+          type="outline"
+          style={{
+            height: '64px',
+            width: '100%',
+            fontSize: '16px',
+            fontWeight: 600,
+            backgroundColor: colors.accent[800],
+            color: colors.white
+          }}
+        >
+          {t('signUp.success.next')}
+        </Button>
+      </Flex>
+    </>
+  );
+};
+
+export default Page;
--- src/i18n/locales/ja/auth-page.ts
+++ src/i18n/locales/ja/auth-page.ts
@@ -1,6 +1,10 @@
 const signUp = {
   title: 'CULUMUリサーチから２パターンの依頼が届きます！会員登録して、好きな依頼に参加しましょう！',
-  aa: 'ss'
+  success: {
+    title: '会員登録が完了しました！',
+    description: '案件の依頼はLINEで届きます。\nご依頼までお待ちください',
+    next: '次へ進む',
+  }
 };
 
 export default {

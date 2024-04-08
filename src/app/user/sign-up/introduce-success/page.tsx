bacd3e75de21c325e3bf4efed9b4cd9fb335528f
xiayang <2889917692@qq.com>
2024-04-08 10:55:18 +0800

    change text and add tooltip

--- src/app/user/sign-up/introduce-success/page.tsx
+++ src/app/user/sign-up/introduce-success/page.tsx
@@ -1,56 +1,45 @@
 'use client';
 
-import { Flex, Typography, Image } from 'antd';
+import { Flex, Typography, Image, Tooltip } from 'antd';
 import { useRouter } from 'next/navigation';
 import React from 'react';
+import styled from 'styled-components';
 
-import colors from '@/theme/colors';
 import { useTranslation } from '@/i18n/client';
 
-import { Button } from '../../../components/antd';
+import colors from '@/theme/colors';
 
 const Page: React.FC = () => {
   const { t } = useTranslation('auth-page');
-  const router = useRouter();
 
   return (
-    <>
-      <Flex vertical justify="center" align="center">
-        <Typography.Title style={{ fontSize: '22px', margin: '64px 0 16px' }}>
-          {t('signUp.introduceSuccess.title')}
-        </Typography.Title>
-        <Typography.Text style={{ whiteSpace: 'pre-wrap', textAlign: 'center', fontSize: '16px' }}>
-          {t('signUp.introduceSuccess.description')}
-        </Typography.Text>
-        <Image src="/images/badge.png" style={{ marginLeft: '-10%', marginTop: '62px' }} alt={''} />
-      </Flex>
-      <Flex
-        style={{
-          position: 'fixed',
-          bottom: 0,
-          width: '100%',
-          borderTop: '1px solid #D9D9D9',
-          padding: '16px 20px',
-          boxSizing: 'border-box',
-          backgroundColor: colors.white
+    <Flex vertical justify="center" align="center">
+      <Tooltip
+        placement="bottomRight"
+        title={'タップで画面を閉じる'}
+        defaultOpen={true}
+        color={colors.white}
+        overlayInnerStyle={{
+          backgroundColor: colors.white,
+          color: colors.black,
+          fontSize: 17,
+          fontWeight: 600,
+          padding: '11px 15px'
         }}
       >
-        <Button
-          type="outline"
-          onClick={() => router.push('/')}
-          style={{
-            height: '64px',
-            width: '100%',
-            fontSize: '16px',
-            fontWeight: 600,
-            backgroundColor: colors.accent[800],
-            color: colors.white
-          }}
-        >
-          {t('signUp.introduceSuccess.returnToLine')}
-        </Button>
-      </Flex>
-    </>
+        <div style={{ position: 'absolute', right: 20, top: 0 }} />
+      </Tooltip>
+      <Typography.Title style={{ fontSize: '22px', margin: '80px 0 16px' }}>
+        {t('signUp.introduceSuccess.title')}
+      </Typography.Title>
+      <Typography.Text style={{ whiteSpace: 'pre-wrap', textAlign: 'center', fontSize: '16px' }}>
+        {t('signUp.introduceSuccess.description')}
+      </Typography.Text>
+      <Image src="/images/badge.png" style={{ marginTop: '62px' }} alt={''} />
+      <Typography.Text style={{ whiteSpace: 'pre-wrap', textAlign: 'center', fontSize: '16px', marginTop: 16 }}>
+        {t('signUp.introduceSuccess.tips')}
+      </Typography.Text>
+    </Flex>
   );
 };
 
